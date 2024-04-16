a44973457874475cfa5f055f53168dacc8b5ad7e
fralcala <fralcala@iu.edu>
2024-04-16 10:02:20 -0400

    added to readme

--- app/app.js
+++ app/app.js
@@ -1,19 +1,14 @@
 function initListeners() {
     $("nav a").on("click", (e) => {
         let btnID = e.currentTarget.id;
-        // console.log(pageContent);
-        // $("#app").html(eval(pageContent));
         loadContent(btnID);
     } );
 }
 
 function loadContent(pageName) {
-    // let pageContent = pageName +"Content";
     $.get(`pages/${pageName}.html`, (data) =>{
-        // console.log(data);
         $("#app").html(data);
     }).fail(function (error) {
-        // console.log("error",error);
         alert("Page is not " + error.statusText)
     });
     
@@ -25,7 +20,6 @@ function testButton(pageName) {
 }
  
 $(document).ready(function () {
-// $("#app").html(homeContent);
 loadContent("home")
 initListeners();
 });
